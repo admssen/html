a05db77b3eff788605af56d1ea601f42212c6860
admssen <wisenorwegian@gmail.com>
2025-03-20 21:30:39 +0500

    ingenious

--- scripts/sender.js
+++ scripts/sender.js
@@ -3,7 +3,7 @@ function updateSender() {
     if (sender==""){
         document.getElementById('sender').innerHTML="[unknown]";
     } else{
-        if (sender.length > 8) { sender = sender.slice(0, 8); }
+        if (sender.length > 26) { sender = sender.slice(0, 26); }
         document.getElementById('sender').innerHTML="["+sender+"]";
     }
 }
