e5ea8149363a748c17dd0bd22d68596c01969a2c
admssen <wisenorwegian@gmail.com>
2025-03-23 22:21:28 +0500

    ski

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,6 +1,4 @@
 function copyAddress() {
     var to_clipboard = document.getElementById("ip");
-    to_clipboard.select();
-    to_clipboard.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(to_clipboard.value);
 }
